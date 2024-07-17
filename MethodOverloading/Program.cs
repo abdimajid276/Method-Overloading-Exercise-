8787d19b7a59d685f543f051db2a7100c981872f
Abdimajid <abdimajid254@gmail.com>
2024-07-17 17:07:14 -0400

    changed isCurrency to false

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,35 @@
 {
     public class Program
     {
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static string Add(int numOne, int numTwo, bool IsCurrency)
+        {
+            var sum = numOne + numTwo;
+            if (IsCurrency && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (IsCurrency && sum == 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+    }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(1 , 1, IsCurrency:true));
         }
     }
 }
